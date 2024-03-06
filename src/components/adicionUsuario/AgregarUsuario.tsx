b6011825867ebc813ee69a2ad37d9e0a4841fb82
VictorAponteParedes <vaponte520@gmail.com>
2024-03-05 23:40:17 -0300

    Agrega: configuracion de estilos de fuentes y estado en linea del usuario.

--- src/components/adicionUsuario/AgregarUsuario.tsx
+++ src/components/adicionUsuario/AgregarUsuario.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
+import { useFonts, Ubuntu_400Regular } from "@expo-google-fonts/ubuntu";
 
 export const AgregarUsuario = () => {
+  const [loraLoading] = useFonts({
+    Ubuntu_400Regular,
+  });
+
+  if (!loraLoading) {
+    return null;
+  }
+
   const usuarios = [
     { nombre: "Lorena", imagen: require("../../img/perfil.jpg") },
     { nombre: "Carlos", imagen: require("../../img/perfil_2.jpg") },
@@ -17,10 +26,40 @@ export const AgregarUsuario = () => {
           marginTop: 85,
           marginBottom: 10,
           fontSize: 14,
-          fontWeight: "bold",
+          fontFamily: "Ubuntu_400Regular",
         }}>
         Recent
       </Text>
+    );
+  };
+
+  const renderUsuario = (usuario, index) => {
+    return (
+      <View key={index} style={{ alignItems: "center", marginRight: 10 }}>
+        <Image
+          source={usuario.imagen}
+          style={{ width: 65, height: 65, borderRadius: 50 }}
+        />
+        {/* Pelotita verde encima de la imagen */}
+        {(index === 0 || index === 2 || index == 3) && (
+          <View
+            style={{
+              position: "absolute",
+              backgroundColor: "green",
+              width: 15,
+              height: 15,
+              borderRadius: 7.5,
+              borderWidth: 2,
+              borderColor: "white",
+              top: 0,
+              right: 4,
+            }}
+          />
+        )}
+        <Text style={{ marginTop: 5, fontFamily: "Ubuntu_400Regular" }}>
+          {usuario.nombre}
+        </Text>
+      </View>
     );
   };
 
@@ -35,19 +74,16 @@ export const AgregarUsuario = () => {
         }}>
         <View style={{ alignItems: "center", marginRight: 10 }}>
           <Icon name="add-circle" solid size={65} />
-          <Text style={{ marginTop: 5, fontSize: 14, fontWeight: "bold" }}>
+          <Text
+            style={{
+              marginTop: 5,
+              fontSize: 14,
+              fontFamily: "Ubuntu_400Regular",
+            }}>
             Add new
           </Text>
         </View>
-        {usuarios.map((usuario, index) => (
-          <View key={index} style={{ alignItems: "center", marginRight: 10 }}>
-            <Image
-              source={usuario.imagen}
-              style={{ width: 65, height: 65, borderRadius: 50 }}
-            />
-            <Text style={{ marginTop: 5 }}>{usuario.nombre}</Text>
-          </View>
-        ))}
+        {usuarios.map((usuario, index) => renderUsuario(usuario, index))}
       </View>
     </>
   );
