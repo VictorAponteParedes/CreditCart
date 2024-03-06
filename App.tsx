d031ee0e2795265a30c9ff61a55a6fb944508bb3
VictorAponteParedes <vaponte520@gmail.com>
2024-03-05 23:36:31 -0300

    Agrega: configuracion de stilos de fuente

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import { useFonts, Oswald_300Light } from "@expo-google-fonts/oswald";
 import { Home } from "./src/screens/home/Home";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 export default function App() {
+  const [oswaldLoaded] = useFonts({
+    Oswald_300Light,
+  });
+
+  if (!oswaldLoaded) {
+    return null;
+  }
   const Stack = createStackNavigator();
   return (
     <NavigationContainer>
